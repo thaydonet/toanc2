cff39981a4c0c24235e1cb356e5e2d2eb95353cc
thaydonet <thaydo.net@gmail.com>
2026-03-14 21:52:51 +0700

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://toanc2.pages.dev',
   integrations: [
     react(),
     mdx({
@@ -13,14 +15,21 @@ export default defineConfig({
         singleDollarTextMath: true,
         inlineMathDouble: false
       }]],
-      rehypePlugins: [rehypeKatex],
+      rehypePlugins: [[rehypeKatex, {
+        strict: "ignore",
+        throwOnError: false
+      }]],
     }),
+    sitemap(),
   ],
   markdown: {
     remarkPlugins: [[remarkMath, { 
       singleDollarTextMath: true,
       inlineMathDouble: false
     }]],
-    rehypePlugins: [rehypeKatex],
+    rehypePlugins: [[rehypeKatex, {
+      strict: "ignore",
+      throwOnError: false
+    }]],
   },
 });
